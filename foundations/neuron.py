feb3b9245e137115204d65f68540e3145d63ae1b
Zacharias Escalante <52757009+Zescalante@users.noreply.github.com>
2026-09-07 11:16:49 -0400

    Implement Single neuron forward pass (Build a Neural Net 1/5)

---
+++ foundations/neuron.py
@@ -0,0 +1,21 @@
+import numpy as np
+from numpy.typing import NDArray
+
+
+class Solution:
+    def forward(self, x: NDArray[np.float64], w: NDArray[np.float64], b: float, activation: str) -> float:
+        # x: 1D input array
+        # w: 1D weight array (same length as x)
+        # b: scalar bias
+        # activation: "sigmoid" or "relu"
+        # Pre-activation: z = dot(x, w) + b
+        # Sigmoid: σ(z) = 1 / (1 + exp(-z))
+        # ReLU: max(0, z)
+        z = np.dot(x, w) + b
+
+        if activation == 'relu':
+            return round(max(0.0, z), 5)
+        if activation == 'sigmoid':
+            # z = np.clip(z , 1e-7, 1 - 1e-7)
+            return round(1 / (1 + np.exp(-z)), 5)
+
